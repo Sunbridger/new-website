7ced7f8f53318b5854c4d1327d11d0d3c971b3f8
Sunbridger <739272884@qq.com>
2020-09-17 15:23:47 +0800

    feat: add 埋点接口

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@ export default {
     },
     methods: {
         trackFn() {
-            get('sendmaidian', {
+            get('/sendmaidian', {
                 ua: navigator.userAgent
             });
         }
